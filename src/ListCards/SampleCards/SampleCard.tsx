8f009f16473c582adb576f0b6095247ea1c76328
maple-fm <fuma.deguchi@gmail.com>
2023-10-30 15:19:16 +0900

    update: User側のコンポーネントディレクトリの作成

--- src/ListCards/SampleCards/SampleCard.tsx
+++ src/ListCards/SampleCards/SampleCard.tsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import ReactCardFlip from "react-card-flip";
-import { Footer } from "../../components/Footer";
-import { Interest } from "../../components/Interest";
-import { About } from "../../components/About";
-import { Info } from "../../components/Info";
+import { Footer } from "../../components/User/Footer";
+import { Interest } from "../../components/User/Interest";
+import { About } from "../../components/User/About";
+import { Info } from "../../components/User/Info";
 import "./SampleCard.css";
 import { AmatechInfo } from "../../components/Amatech/AmatechInfo";
 import { Events } from "../../components/Amatech/Events";
-import { Hobby } from "../../components/Hobby";
+import { Hobby } from "../../components/User/Hobby";
 
+// 名刺の名前を変更してみましょう！
+// hint: SampleCardを好きな名前に変更してください！
 const SampleCard = () => {
   const [isFlip, setIsFlip] = useState(false);
 
@@ -23,7 +25,6 @@ const SampleCard = () => {
           {/* 個人の名刺 */}
           <div className="front" onClick={() => handleClick()}>
             <section className="main__section">
-
               {/* 名前と学年を変更してみましょう！ */}
               <Info
                 name="あまてく太郎"
@@ -33,9 +34,7 @@ const SampleCard = () => {
               />
 
               {/* 自分自身ついての自己紹介文を書いてみましょう！ */}
-              <About
-                aboutText="大学では情報科学を学んでおり、特にデータ解析と機械学習に興味を持って研究しています。"
-              />
+              <About aboutText="大学では情報科学を学んでおり、特にデータ解析と機械学習に興味を持って研究しています。" />
 
               {/* あなたの好きなことは何ですか？毎日よくしていることを書いてみましょう！ */}
               <Hobby hobbyText="旅行と写真撮影と読書が趣味です！特に台湾の文化や風景が好きです。休日には都市の風景や友人たちを撮影しています。最近はミステリーや東野圭吾の本を読んでいます。" />
---
+++ src/components/User/Info.tsx
@@ -0,0 +1,46 @@
+import LazyLoad from "react-lazy-load";
+import YoutubeIcon from "@mui/icons-material/YouTube";
+
+type Props = {
+  name: string;
+  label: string;
+  knowMoreLink?: string;
+  mail: string;
+  YoutubeLink?: string;
+};
+
+export const Info = (props: Props) => {
+  const { name, label, knowMoreLink, YoutubeLink } = props;
+  return (
+    <>
+      <LazyLoad height={300}>
+        <img src="./images/ogp.PNG" alt="プロフィール画像" />
+      </LazyLoad>
+      <section className="section-info">
+        <h2 className="name">{name}</h2>
+        <p className="stack">{label}</p>
+        {knowMoreLink && (
+          <small>
+            <a href={knowMoreLink} target="_" rel="noopener noreferrer">
+              Know more
+            </a>
+          </small>
+        )}
+        <div className="contact">
+          {/* <MailTo email={mail} subject="お話しましょう！" body="こんにちは！あまてく！" className="mailto">
+                        <EmailIcon className="icon" fontSize="small" />
+                        <span>Email</span>
+                    </MailTo> */}
+          {YoutubeLink && (
+            <div className="Youtube">
+              <a href={YoutubeLink} target="_" rel="noopener noreferrer">
+                <YoutubeIcon className="icon" fontSize="small" />
+                <span>Youtube</span>
+              </a>
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
+};
