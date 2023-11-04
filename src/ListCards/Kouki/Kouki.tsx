377bf20f6bb89bbdf9d3db1879e36292ea2a1c97
kitsune <67742985+yk-mt12@users.noreply.github.com>
2023-11-04 11:57:03 +0900

    e

--- src/ListCards/Kouki/Kouki.tsx
+++ src/ListCards/Kouki/Kouki.tsx
@@ -30,27 +30,23 @@ const Kouki = () => {
             <section className="main__section">
               {/* 入門編: 名前と学年を変更してみましょう！ */}
               <Info
-                name="あまてく太郎"
-                label="情報科学部・3年"
-                mail=""
-                YoutubeLink="https://www.youtube.com/watch?v=gdZLi9oWNZg"
+                name="本多 光輝"
+                label="理学部・４年"
+                
               />
               {/* 入門編: 自分自身ついての自己紹介文を書いてみましょう！ */}
-              <About aboutText="大学では情報科学を学んでおり、特にデータ解析と機械学習に興味を持って研究しています。" />
+              <About aboutText="大学では天文学を学んでいます。" />
 
               {/* 入門編: あなたの好きなことは何ですか？毎日よくしていることを書いてみましょう！ */}
-              <Hobby hobbyText="旅行と写真撮影と読書が趣味です！特に台湾の文化や風景が好きです。休日には都市の風景や友人たちを撮影しています。最近はミステリーや東野圭吾の本を読んでいます。" />
+              <Hobby hobbyText="ゲームをよくしています。" />
 
               {/* 入門編: あなたの好きなことは何ですか？毎日よくしていることを書いてみましょう！ */}
-              <Interest interestText="人工知能と社会についての勉強、モバイルアプリケーション、デザイン思考についての勉強に興味があります。" />
+              <Interest interestText="毎日やっているわけではないのですが、最近はUnityをよくいじっています。" />
 
               {/* 入門編: 自己紹介用のリンクがあれば変更してみましょう！ */}
               {/* リンクがない方はURLを削除してください */}
               <Footer
-                twitterLink="https://twitter.com/amatech1006"
-                githubLink="https://github.com/amatech-repo"
-                instagramLink=""
-                linkedinLink=""
+                twitterLink="https://twitter.com/mistltoe08"
               />
             </section>
           </div>
