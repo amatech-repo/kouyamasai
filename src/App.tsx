859fb8c6ad8d2b4e2c00b7bbeca02180ff4850fa
kitsune <67742985+yk-mt12@users.noreply.github.com>
2023-11-04 10:56:59 +0900

    松石コンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/samplecards" element={<SampleCard />} />
         <Route path="/fuma" element={<Fuma />} />
+        <Route path="/matsuishi" element={<Matsuishi />} />
       </Routes>
     </BrowserRouter>
   );
