ee6296f26fae05035abe95ed013760e8413ec2d1
maple-fm <fuma.deguchi@gmail.com>
2023-11-04 09:05:23 +0900

    fuma-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SampleCard from "./ListCards/SampleCards/SampleCard.tsx";
+// 以下の参考例をコピーし、**を修正してください
+// 参考例: import ** from "./ListCards/**/**/.tsx";
+import Fuma from "./ListCards/Fuma/Fuma.tsx";
 import Home from "./Pages/Home.tsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* サンプルを参考にルータをコピペしてください */}
+        {/* 以下の参考例をコピーし、**を修正してください */}
+        {/* 参考例: <Route path="/**" element={<** />} /> */}
         <Route path="/" element={<Home />} />
         <Route path="/samplecards" element={<SampleCard />} />
+        <Route path="/fuma" element={<Fuma />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/ListCards/Fuma/User/Info.tsx
@@ -0,0 +1,45 @@
+import LazyLoad from "react-lazy-load";
+import YoutubeIcon from "@mui/icons-material/YouTube";
+
+type Props = {
+  name: string;
+  label: string;
+  knowMoreLink?: string;
+  mail: string;
+  YoutubeLink?: string;
+};
+
+export const Info = (props: Props) => {
+  const { name, label, knowMoreLink, YoutubeLink } = props;
+  return (
+    <>
+      <LazyLoad height={300}>
+        {/* ogp.PNGを新しく追加した画像に変更してください */}
+        {/* 例: ./images/**.png */}
+        <img src="./images/ogp.PNG" alt="プロフィール画像" />
+      </LazyLoad>
+      <section className="section-info">
+        <h2 className="name">{name}</h2>
+        <p className="stack">{label}</p>
+        {knowMoreLink && (
+          <small>
+            <a href={knowMoreLink} target="_" rel="noopener noreferrer">
+              Know more
+            </a>
+          </small>
+        )}
+        <div className="contact">
+          {YoutubeLink && (
+            <div className="Youtube">
+              <a href={YoutubeLink} target="_" rel="noopener noreferrer">
+                <YoutubeIcon className="icon" fontSize="small" />
+                <span>Youtube</span>
+              </a>
+            </div>
+          )}
+          {/* 応用編: 好きなサイトに飛べるボタンを作成しよう */}
+        </div>
+      </section>
+    </>
+  );
+};
