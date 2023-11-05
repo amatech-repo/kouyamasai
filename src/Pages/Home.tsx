ee5bd6d597f17c2015f1f584cebd8528c207b965
あまてく <132275270+amatech1006@users.noreply.github.com>
2023-11-05 12:17:32 +0900

    fix

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -24,7 +24,7 @@ const Home = () => {
     { path: "Oriduru", name: "おりづる" },
     { path: "Koncha", name: "こんちゃ" },
     { path: "Ren", name: "Ren" },
-    { path: 'Ryuki', name: 'サンプル' },
+    { path: 'Ryuki', name: 'りゅうき' },
 
   ];
 
