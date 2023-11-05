fab115f08f53d8f0f6a59f513ec189b554bceb04
あまてく <132275270+amatech1006@users.noreply.github.com>
2023-11-05 13:41:55 +0900

    入門編

--- src/ListCards/Testukun/Testukun.tsx
+++ src/ListCards/Testukun/Testukun.tsx
@@ -30,25 +30,25 @@ const Testukun = () => {
             <section className="main__section">
               {/* 入門編: 名前と学年を変更してみましょう！ */}
               <Info
-                name="あまてく太郎"
-                label="情報科学部・3年"
+                name="てつくん"
+                label="地理学科・3年"
                 mail=""
                 YoutubeLink="https://www.youtube.com/watch?v=gdZLi9oWNZg"
               />
               {/* 入門編: 自分自身ついての自己紹介文を書いてみましょう！ */}
-              <About aboutText="大学では情報科学を学んでおり、特にデータ解析と機械学習に興味を持って研究しています。" />
+              <About aboutText="大学では交通地理を学んでおり、鉄道と都市に興味を持って研究しています。" />
 
               {/* 入門編: あなたの趣味はなんですか？*/}
-              <Hobby hobbyText="旅行と写真撮影と読書が趣味です！特に台湾の文化や風景が好きです。休日には都市の風景や友人たちを撮影しています。最近はミステリーや東野圭吾の本を読んでいます。" />
+              <Hobby hobbyText="鉄道旅行と散歩が趣味です。特に日本の鉄道が好きです。休日には都市の風景や友人たちを撮影しています。最近はミステリーや東野圭吾の本を読んでいます。" />
 
               {/* 入門編: あなたが今一番興味を持っていることを書いてみましょう！ */}
-              <Interest interestText="人工知能と社会についての勉強、モバイルアプリケーション、デザイン思考についての勉強に興味があります。" />
+              <Interest interestText="台湾の鉄道や文化に興味があります。" />
 
               {/* 入門編: 自己紹介用のリンクがあれば変更してみましょう！ */}
               {/* リンクがない方はURLを削除してください */}
               <Footer
-                twitterLink="https://twitter.com/amatech1006"
-                githubLink="https://github.com/amatech-repo"
+                twitterLink=""
+                githubLink=""
                 instagramLink=""
                 linkedinLink=""
               />
