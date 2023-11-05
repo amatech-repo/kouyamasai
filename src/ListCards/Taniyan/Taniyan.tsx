661203ce8a350273237e1bb16e02d7062a162e75
あまてく <132275270+amatech1006@users.noreply.github.com>
2023-11-05 15:53:39 +0900

    入門編

--- src/ListCards/Taniyan/Taniyan.tsx
+++ src/ListCards/Taniyan/Taniyan.tsx
@@ -30,26 +30,26 @@ const Taniyan = () => {
             <section className="main__section">
               {/* 入門編: 名前と学年を変更してみましょう！ */}
               <Info
-                name="あまてく太郎"
-                label="情報科学部・3年"
+                name="TANIYAN"
+                label="理学部・2年"
                 mail=""
                 YoutubeLink="https://www.youtube.com/watch?v=gdZLi9oWNZg"
               />
               {/* 入門編: 自分自身ついての自己紹介文を書いてみましょう！ */}
-              <About aboutText="大学では情報科学を学んでおり、特にデータ解析と機械学習に興味を持って研究しています。" />
+              <About aboutText="数理科学科として日々数学に励んでいます…たぶん。" />
 
               {/* 入門編: あなたの趣味はなんですか？*/}
-              <Hobby hobbyText="旅行と写真撮影と読書が趣味です！特に台湾の文化や風景が好きです。休日には都市の風景や友人たちを撮影しています。最近はミステリーや東野圭吾の本を読んでいます。" />
+              <Hobby hobbyText="数学が趣味！特に長期休暇中は友人と自主ゼミを開いて勉強してます！" />
 
               {/* 入門編: あなたが今一番興味を持っていることを書いてみましょう！ */}
-              <Interest interestText="人工知能と社会についての勉強、モバイルアプリケーション、デザイン思考についての勉強に興味があります。" />
+              <Interest interestText="数学の分野で特に多様体というものに興味があります！" />
 
               {/* 入門編: 自己紹介用のリンクがあれば変更してみましょう！ */}
               {/* リンクがない方はURLを削除してください */}
               <Footer
-                twitterLink="https://twitter.com/amatech1006"
-                githubLink="https://github.com/amatech-repo"
-                instagramLink=""
+                twitterLink="https://twitter.com/TT2949291"
+                githubLink=""
+                instagramLink="https://instagram.com/tnymtig2"
                 linkedinLink=""
               />
             </section>
