a4efff63ae7cc4c1e3f62f26492884dad49a0f53
あまてく <132275270+amatech1006@users.noreply.github.com>
2023-11-06 05:17:11 +0000

    l

--- src/ListCards/Kawa/Kawa.tsx
+++ src/ListCards/Kawa/Kawa.tsx
@@ -4,15 +4,15 @@ import { Footer } from "./User/Footer";
 import { Interest } from "./User/Interest";
 import { About } from "./User/About";
 import { Info } from "./User/Info";
-import "./SampleCard.css";
+import "./Kawa.css";
 import { Hobby } from "./User/Hobby";
 import { Events } from "../../components/Amatech/Events";
 import { AmatechInfo } from "../../components/Amatech/AmatechInfo";
 
 // ファイル名と合わせてください！※先頭は大文字で
-// 例 SampleCard
+// 例 Kawa
 // この時、右クリックからファイル名の一括置換(全ての出現箇所を変更)を行うと楽です！
-const SampleCard = () => {
+const Kawa = () => {
   const [isFlip, setIsFlip] = useState(false);
 
   const handleClick = () => {
@@ -83,4 +83,4 @@ const SampleCard = () => {
   );
 };
 
-export default SampleCard;
+export default Kawa;
